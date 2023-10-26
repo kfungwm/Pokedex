b64312d54f259f6225f24d3413c6f65c41294900
kfungwm <kevinfung87@gmail.com>
2023-10-27 00:52:52 +0200

    api call to all te pokemon

---
+++ src/components/Pokemon.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import React, { useState, useRef, useEffect } from 'react'
+import axios from 'axios'
+
+function Pokemon() {
+  // const [allPokemon, setAllPokemon] = useState([])
+
+  // useEffect(() => {
+  //   axios
+  //     .get('https://pokeapi.co/api/v2/pokemon/1')
+  //     .then(async (response) => {
+  //       setAllPokemon(response.data)
+  //       console.log(response.data)
+  //     })
+  //     .catch((error) => {
+  //       console.log('can not found')
+  //     })
+  // }, [])
+
+  return <div></div>
+}
+
+export default Pokemon
